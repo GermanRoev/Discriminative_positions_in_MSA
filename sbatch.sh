be09e56ea554715a853a427597fd8f98d2d554b8
GermanRoev <117848398+GermanRoev@users.noreply.github.com>
2023-06-13 23:55:50 +0300

    Add files via upload

---
+++ sbatch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -o stdout
+#SBATCH -e stderr
+#SBATCH -J roev_german
+#SBATCH --get-user-env
+#SBATCH --time=01:00:00
+#SBATCH -N 1 
+#SBATCH -n 1 
+#SBATCH --mem=1000
+#SBATCH -p batch
+ 
+pip3 install -U scikit-learn
+pip3 install numpy
+
+echo 'START'
+python3 search.py  --input hbv_sgene_mafft.fa --output hbv_sgene_output.txt
+echo 'FINISH'
